a896b4060c8eae79d1c0f9d6ee37ae310b280e5a
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-05-10 09:48:41 -0400

    Modify arcade example

--- read_bit_arcade.py
+++ read_bit_arcade.py
@@ -28,16 +28,24 @@ def update(delta_time):
     global microbit, player_x, player_y
     # 0 - WIDTH
 
-    new_x = utils.map_value(microbit.accelerometer.get_x(), -1000, 1000, 0, WIDTH)
-    new_y = utils.map_value(microbit.accelerometer.get_y(), -1000, 1000, HEIGHT, 0)
+    # Get microbit data
+    accelerometer_x = microbit.accelerometer.get_x()
+    accelerometer_y = microbit.accelerometer.get_y()
 
+    # set player location to the microbit x and y location
+    # map_value() translates x and y values to the size of the arcade window
+    # The range of -1000 to 1000 from the microbit gets translated to
+    # a range of 0 to WIDTH to fit our arcade window
+    player_x = utils.map_value(accelerometer_x, -1000, 1000, 0, WIDTH)
+    player_y = utils.map_value(accelerometer_y, -1000, 1000, HEIGHT, 0)
+
+    # is_pressed() checks if the button is being held.
+    if microbit.button_a.is_pressed():
+        print("button a is being held")
+
+    # was_pressed() ensures only a single trigger
     if microbit.button_a.was_pressed():
-        print("hello")
-
-    if abs(new_x - player_x) > 15:
-        player_x = new_x
-    if abs(new_y - player_y) > 15:
-        player_y = new_y
+        print("button a was pressed down")
 
 
 @window.event
